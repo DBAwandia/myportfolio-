9dbe58fc06128e5b3dc6cc1c2d7167fd6786c852
ken <kmaina510@gmail.com>
2023-02-03 19:26:34 +0300

    added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,7 +6,7 @@ import TrackVisibility from 'react-on-screen';
 
 
 function About() {
-  let image="https://media.licdn.com/dms/image/D4D03AQFNymxm28bW-g/profile-displayphoto-shrink_200_200/0/1675019660312?e=1680739200&v=beta&t=pmhbuxC_h6fGe3MIZAOcKg_TBVMkFzam_E1M22LYlBA"
+  let image="/images/portfolio.png"
   return (
     <div className="About" id='home'>
       <div className='About_container'>
